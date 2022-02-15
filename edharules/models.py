9e7b4653d748b48db9049a530bc77baf19fcf32a
notaigbe <notaigbe@gmail.com>
2022-02-15 12:43:05 +0100

    Rearranged rules presentation

--- edharules/models.py
+++ edharules/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class EDHARule(models.Model):
-    rule_section_num = models.IntegerField
+    rule_section_num = models.IntegerField(default=1)
     rule_section_title = models.CharField(max_length=255)
     rule_subsection_num = models.CharField(max_length=2)
     rule_subsection_title = models.CharField(max_length=255)
