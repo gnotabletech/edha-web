0d42c9ac03d5432771ec5aa3c06ba83c1acb2951
notaigbe <notaigbe@gmail.com>
2022-02-27 23:14:33 +0100

    Added new url to allowed hosts

--- edharulesandbiz/settings.py
+++ edharulesandbiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3mb!8zstg0vma!p+kb#2o@!1%n406!yrec_xg8kgow3wc7e8ej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'edharulesandbiz.herokuapp.com', 'localhost', 'edha-gov.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'edharulesandbiz.herokuapp.com', 'localhost', 'edha-gov.herokuapp.com','edha2-gov.herokuapp.com']
 
 # Application definition
 
