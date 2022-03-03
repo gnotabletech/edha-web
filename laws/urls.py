0b92e39cf5f008526b81ea8cf45d3d0ac2660af5
notaigbe <notaigbe@gmail.com>
2022-03-03 06:38:37 +0100

    removed whitenoise compressed storage

--- laws/urls.py
+++ laws/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,5 +8,5 @@ urlpatterns = [
     path('', views.laws_repo, name='laws'),
     path('getlaw', views.getlaw, name='getlaw'),
     path('display_law/<value>', views.display_law, name='display_law')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
