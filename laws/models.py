eecc76881a7b189ce31f45be28083ff014892729
notaigbe <notaigbe@gmail.com>
2022-03-04 10:37:52 +0100

    Staging Secure MVP

--- laws/models.py
+++ laws/models.py
@@ -40,7 +40,7 @@ class BillsAndLaws(models.Model):
         db_table = 'Bills_and_Laws'
 
     def save(self, *args, **kwargs):
-        self.document = f'{self.short_title}.pdf'
+        self.document = f'{self.short_title}.pdf'.replace('/', '_')
         super().save(*args, **kwargs)
 
     def __str__(self):
