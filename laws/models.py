fadfa43e8b42b0c0aa4ece3d9ed1a3e7350ecd5d
notaigbe <notaigbe@gmail.com>
2022-03-04 11:41:04 +0100

    debugging media file upload location

--- laws/models.py
+++ laws/models.py
@@ -5,8 +5,20 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+import os
+
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.utils import timezone
+
+from edharulesandbiz import settings
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name):
+        if self.exists(name):
+            os.remove(os.path.join(settings.DEFAULT_FILE_STORAGE, name))
+        return name
 
 
 class BillsAndLaws(models.Model):
@@ -33,15 +45,17 @@ class BillsAndLaws(models.Model):
     publication = models.DateField(db_column='PUBLICATION', blank=True, null=True)  # Field name made lowercase.
     short_title = models.TextField(db_column='SHORT_TITLE', max_length=100, blank=True,
                                    null=True)  # Field name made lowercase.
-    document = models.FileField(upload_to='files', default=timezone.now)
+    document = models.FileField(upload_to=settings.DEFAULT_FILE_STORAGE, default=timezone.now)
 
     class Meta:
         # managed = False
         db_table = 'Bills_and_Laws'
 
-    def save(self, *args, **kwargs):
-        self.document = f'{self.short_title}.pdf'.replace('/', '_').replace(') (', '_').replace(') ', '_').replace(' (', '_').replace(', ', '_').replace(' ', '_')
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #    self.document = f'{self.short_title}.pdf'.replace('/', '_').replace(') (', '_').replace(') ', '_').replace(' (',
+    #                                                                                                               '_').replace(
+    #        ', ', '_').replace(' ', '_')
+    #    super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
