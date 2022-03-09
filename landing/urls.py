6fde8988784f9e50a1abfb094f0482d85c08af00
notaigbe <notaigbe@gmail.com>
2022-03-09 23:10:00 +0100

    pwa test - bypass django-pwa

--- landing/urls.py
+++ landing/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.index, name='home')
+    path('home', views.index, name='home'),
+    re_path(r'^serviceworker(.*.js)$', views.serviceworker, name='serviceworker'),
+    re_path(r'^manifest\.json$', views.manifest, name='manifest'),
+    re_path('^offline/$', views.offline, name='offline')
 ]
 
--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template.loader import get_template
+
+from edharulesandbiz import settings
 
 
-def serviceworker(request, js):
-    template = get_template('serviceworker.js')
-    html = template.render()
-    return HttpResponse(html, content_type="application/x-javascript")
+def serviceworker(request):
+    response = HttpResponse(open(settings.PWA_SERVICE_WORKER_PATH).read(), content_type='application/javascript')
+    return response
+
+
+def manifest(request):
+    return render(request, 'manifest.json', {
+        setting_name: getattr(settings, setting_name)
+        for setting_name in dir(settings)
+        if setting_name.startswith('PWA_')
+    }, content_type='application/json')
+
+
+def offline(request):
+    return render(request, "offline.html")
 
 
 # Create your views here.
