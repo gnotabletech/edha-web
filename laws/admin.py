2f91b622d90a889a7db98671cf9eacaed1a9c49a
notaigbe <notaigbe@gmail.com>
2022-03-11 20:42:18 +0100

    customize admin interface

--- laws/admin.py
+++ laws/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
+
 from .models import BillsAndLaws, AdminInfo
 
 
 class BillsAndLawsAdmin(admin.ModelAdmin):
     search_fields = ['title', 'short_title']
     fields = ['title', 'short_title', 'stage_key', 'sponsor', 'first_reading', 'second_reading', 'committee_date', 'referred_committee', 'third_reading', 'assent_date', 'document']
-    list_filter = ['stage_key', 'assent_date']
+    list_filter = ['stage_key', 'assent_date', ('assent_date', DateRangeFilter)]
     list_display = ['title', 'stage_key', 'assent_date']
 
 
