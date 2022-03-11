b182969e8b5166db1bcab41aa841a4ac220c110d
notaigbe <notaigbe@gmail.com>
2022-03-11 15:02:00 +0100

    modify admin console for search capability

--- laws/admin.py
+++ laws/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import BillsAndLaws, AdminInfo
 
+
+class BillsAndLawsAdmin(admin.ModelAdmin):
+    search_fields = ['TITLE', 'SHORT_TITLE']
+
+
 # Register your models here.
-admin.site.register(BillsAndLaws)
+admin.site.register(BillsAndLaws, BillsAndLawsAdmin)
 admin.site.register(AdminInfo)
