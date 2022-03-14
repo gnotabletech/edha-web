eec246d3e475dd23f31bfe6a389d2a8eedfa5ca8
notaigbe <notaigbe@gmail.com>
2022-03-14 16:55:14 +0100

    debugging service workers

--- serviceworker.js
+++ serviceworker.js
@@ -3,20 +3,7 @@ self.addEventListener('install', function(event){
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache){
             return cache.addAll([
-                "./static/assets/css/style.css",
-                "./static/assets/css/styles.css",
-                "./static/assets/vendor/aos/aos.css",
-                "./static/assets/vendor/aos/aos.js",
-                "./static/assets/css/floating-menu/style.css",
-                "./static/assets/css/floating-search/style.css",
-                "./static/assets/vendor/css/bootstrap.min.css",
-                "./static/assets/js/bootstrap.min.js",
-                "./static/assets/js/FormValidator.js",
-                "./static/assets/js/jquery-3.2.1.min.map",
-                "./static/assets/js/scripts.js",
-                "./static/assets/js/signup.js",
-                "./static/assets/js/UserHome.js",
-                "./static/assets/mp4/bg1.mp4",
+
                 "./",
 
             ]);
