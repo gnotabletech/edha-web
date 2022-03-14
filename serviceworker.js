6a983c8f702d2908ec18017cd1af17a12a66b924
notaigbe <notaigbe@gmail.com>
2022-03-14 11:33:33 +0100

    updating service worker to exclude forms

--- serviceworker.js
+++ serviceworker.js
@@ -30,31 +30,6 @@ self.addEventListener('install', function(event){
     );
 });
 
-var networkDataReceived = false;
-
-startSpinner();
-
-// fetch fresh data
-var networkUpdate = fetch('/data.json').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  networkDataReceived = true;
-  updatePage(data);
-});
-
-// fetch cached data
-caches.match('/data.json').then(function(response) {
-  if (!response) throw Error("No data");
-  return response.json();
-}).then(function(data) {
-  // don't overwrite newer network data
-  if (!networkDataReceived) {
-    updatePage(data);
-  }
-}).catch(function() {
-  // we didn't get cached data, the network is our last hope:
-  return networkUpdate;
-}).catch(showErrorMessage).then(stopSpinner());
 
 self.addEventListener('fetch', function(event){
     var requestURL = new URL(event.request.url);
