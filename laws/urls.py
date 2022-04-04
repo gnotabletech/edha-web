e7c627144e8ea979d89b799e1f4141bcd7f0266a
notaigbe <notaigbe@gmail.com>
2022-04-04 21:42:42 +0100

    Added laws report print pages

--- laws/urls.py
+++ laws/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.laws_repo, name='laws'),
     path('laws_repo_more/<last_record>', views.laws_repo_more, name='next_laws'),
-    path('getlaw', views.getlaw, name='getlaw')
+    path('getlaw', views.getlaw, name='getlaw'),
+    path('print', views.print_law, name='print'),
+    path('print_report', views.print_law_report, name='print_report'),
+    path('print_pending_bills', views.print_pending_bills, name='print_pending_bills'),
+    path('print_assented_laws', views.print_assented_laws, name='print_assented_laws')
 ]
 
--- laws/views.py
+++ laws/views.py
@@ -16,7 +16,8 @@ from .models import BillsAndLaws, AdminInfo
 def laws_repo(request):
     if request.user.is_authenticated:
         user = request.user
-        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True), F('stage_key').desc(nulls_last=True))[:10]
+        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True),
+                                                   F('stage_key').desc(nulls_last=True))[:10]
         laws_count = BillsAndLaws.objects.all().count()
         pages = BillsAndLaws.objects.all()[:laws_count:10]
         assented_laws_count = BillsAndLaws.objects.filter(stage='ASSENTED TO').count()
@@ -24,7 +25,8 @@ def laws_repo(request):
         awaiting_assent_count = BillsAndLaws.objects.filter(stage='AWAITING ASSENT').count()
         return render(request, 'laws/index.html',
                       {'user': user, 'laws': laws, 'laws_count': laws_count, 'assented_laws_count': assented_laws_count,
-                       'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count, 'pages': pages})
+                       'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count,
+                       'pages': pages})
     else:
         return redirect('login')
 
@@ -32,7 +34,8 @@ def laws_repo(request):
 def laws_repo_more(request, last_record):
     if request.user.is_authenticated:
         user = request.user
-        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True))[int(last_record):int(last_record) + 10]
+        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True))[
+               int(last_record):int(last_record) + 10]
         laws_count = BillsAndLaws.objects.all().count()
         pages = BillsAndLaws.objects.all()[:laws_count:10]
         assented_laws_count = BillsAndLaws.objects.filter(stage='ASSENTED TO').count()
@@ -41,7 +44,8 @@ def laws_repo_more(request, last_record):
         return render(request, 'laws/index.html',
                       {'user': user, 'laws': laws, 'laws_count': laws_count,
                        'assented_laws_count': assented_laws_count,
-                       'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count, 'pages': pages})
+                       'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count,
+                       'pages': pages})
     else:
         return redirect('login')
 
@@ -53,7 +57,9 @@ def getlaw(request):
     if request.user.is_authenticated:
         request.session.get('searchstring')
         user = request.user
-        laws = BillsAndLaws.objects.filter(title__icontains=request.session.get('searchstring')).order_by(F('assent_date').desc(nulls_last=True)) | BillsAndLaws.objects.filter(short_title__icontains=request.session.get('searchstring')).order_by(F('assent_date').desc(nulls_last=True))
+        laws = BillsAndLaws.objects.filter(title__icontains=request.session.get('searchstring')).order_by(
+            F('assent_date').desc(nulls_last=True)) | BillsAndLaws.objects.filter(
+            short_title__icontains=request.session.get('searchstring')).order_by(F('assent_date').desc(nulls_last=True))
         laws_count = BillsAndLaws.objects.all().count()
         assented_laws_count = BillsAndLaws.objects.filter(stage='ASSENTED TO').count()
         pending_laws_count = BillsAndLaws.objects.exclude(stage='ASSENTED TO').count()
@@ -63,3 +69,50 @@ def getlaw(request):
                        'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count})
     else:
         return redirect('login_user_quicksearch')
+
+
+def print_law(request):
+    if request.user.is_authenticated:
+        user = request.user
+        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True),
+                                                   F('stage_key').desc(nulls_last=True))
+        return render(request, 'laws/list.html',
+                      {'user': user, 'laws': laws})
+    else:
+        return redirect('login')
+
+
+def print_law_report(request):
+    if request.user.is_authenticated:
+        user = request.user
+        laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True),
+                                                   F('stage_key').desc(nulls_last=True))
+        title = "DETAILED REPORT OF BILLS AND LAWS"
+        return render(request, 'laws/bills_report.html',
+                      {'user': user, 'laws': laws, 'title': title})
+    else:
+        return redirect('login')
+
+
+def print_pending_bills(request):
+    if request.user.is_authenticated:
+        user = request.user
+        laws = BillsAndLaws.objects.exclude(stage="ASSENTED TO").order_by(F('assent_date').desc(nulls_last=True),
+                                                                          F('stage_key').desc(nulls_last=True))
+        title = "DETAILED REPORT OF PENDING BILLS"
+        return render(request, 'laws/bills_report.html',
+                      {'user': user, 'laws': laws, 'title': title})
+    else:
+        return redirect('login')
+
+
+def print_assented_laws(request):
+    if request.user.is_authenticated:
+        user = request.user
+        laws = BillsAndLaws.objects.filter(stage="ASSENTED TO").order_by(F('assent_date').desc(nulls_last=True),
+                                                                         F('stage_key').desc(nulls_last=True))
+        title = "DETAILED REPORT OF ASSENTED LAWS"
+        return render(request, 'laws/bills_report.html',
+                      {'user': user, 'laws': laws, 'title': title})
+    else:
+        return redirect('login')
