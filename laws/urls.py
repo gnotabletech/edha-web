c1937e969ba506f2d998eb73923aa33511bf1cef
notaigbe <notaigbe@gmail.com>
2022-04-17 11:49:10 +0100

    added chart for assembly stats

--- laws/urls.py
+++ laws/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('print', views.print_law, name='print'),
     path('print_report', views.print_law_report, name='print_report'),
     path('print_pending_bills', views.print_pending_bills, name='print_pending_bills'),
-    path('print_assented_laws', views.print_assented_laws, name='print_assented_laws')
+    path('print_assented_laws', views.print_assented_laws, name='print_assented_laws'),
+    path('get_law_by_assembly', views.get_law_by_assembly, name='get_law_by_assembly')
 ]
 
--- laws/views.py
+++ laws/views.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 from django.contrib.auth import authenticate, login
@@ -6,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.db.models import F
 from django.http import FileResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.views.generic import ListView
 
 from edharulesandbiz.settings import BASE_DIR
@@ -23,6 +25,7 @@ def laws_repo(request):
         assented_laws_count = BillsAndLaws.objects.filter(stage='ASSENTED TO').count()
         pending_laws_count = BillsAndLaws.objects.exclude(stage='ASSENTED TO').count()
         awaiting_assent_count = BillsAndLaws.objects.filter(stage='AWAITING ASSENT').count()
+
         return render(request, 'laws/index.html',
                       {'user': user, 'laws': laws, 'laws_count': laws_count, 'assented_laws_count': assented_laws_count,
                        'pending_laws_count': pending_laws_count, 'awaiting_assent_count': awaiting_assent_count,
@@ -76,8 +79,31 @@ def print_law(request):
         user = request.user
         laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True),
                                                    F('stage_key').desc(nulls_last=True))
+        law_count = []
+        assented_laws_count = []
+        pending_laws_count = []
+        chart_label = []
+        base_year = 1995
+        assemblies = math.ceil((timezone.now().year - base_year) / 4)
+        print(assemblies)
+        for assembly in range(assemblies):
+            assembly_start = base_year + (4 * assembly)
+            assembly_end = assembly_start + 4
+            print(f'{assembly_start} to {assembly_end}')
+
+            law_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                         publication__year__lte=assembly_end).count())
+            pending_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                  publication__year__lte=assembly_end).exclude(
+                stage='ASSENTED TO').count())
+            assented_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                   publication__year__lte=assembly_end).filter(
+                stage='ASSENTED TO').count())
+            chart_label.append(assembly)
+        print(chart_label)
         return render(request, 'laws/list.html',
-                      {'user': user, 'laws': laws})
+                      {'user': user, 'laws': laws, 'law_count': law_count, 'assented_laws_count': assented_laws_count,
+                       'pending_laws_count': pending_laws_count, 'chart_label': chart_label})
     else:
         return redirect('login')
 
@@ -87,9 +113,29 @@ def print_law_report(request):
         user = request.user
         laws = BillsAndLaws.objects.all().order_by(F('assent_date').desc(nulls_last=True),
                                                    F('stage_key').desc(nulls_last=True))
+        law_count = []
+        assented_laws_count = []
+        pending_laws_count = []
+        base_year = 1995
+        assemblies = math.ceil((timezone.now().year - base_year) / 4)
+        print(assemblies)
+        for assembly in range(assemblies):
+            assembly_start = base_year + (4 * assembly)
+            assembly_end = assembly_start + 4
+            print(f'{assembly_start} to {assembly_end}')
+
+            law_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                         publication__year__lte=assembly_end).count())
+            pending_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                  publication__year__lte=assembly_end).exclude(
+                stage='ASSENTED TO').count())
+            assented_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                   publication__year__lte=assembly_end).filter(
+                stage='ASSENTED TO').count())
         title = "DETAILED REPORT OF BILLS AND LAWS"
         return render(request, 'laws/bills_report.html',
-                      {'user': user, 'laws': laws, 'title': title})
+                      {'user': user, 'laws': laws, 'law_count': law_count, 'assented_laws_count': assented_laws_count,
+                       'pending_laws_count': pending_laws_count, 'title': title})
     else:
         return redirect('login')
 
@@ -99,9 +145,30 @@ def print_pending_bills(request):
         user = request.user
         laws = BillsAndLaws.objects.exclude(stage="ASSENTED TO").order_by(F('publication').desc(nulls_last=True),
                                                                           F('stage_key').desc(nulls_last=True))
+        law_count = []
+        assented_laws_count = []
+        pending_laws_count = []
+        base_year = 1995
+        assemblies = math.ceil((timezone.now().year - base_year) / 4)
+        print(assemblies)
+        for assembly in range(assemblies):
+            assembly_start = base_year + (4 * assembly)
+            assembly_end = assembly_start + 4
+            print(f'{assembly_start} to {assembly_end}')
+
+            law_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                         publication__year__lte=assembly_end).count())
+            pending_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                  publication__year__lte=assembly_end).exclude(
+                stage='ASSENTED TO').count())
+            assented_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                   publication__year__lte=assembly_end).filter(
+                stage='ASSENTED TO').count())
+
         title = "DETAILED REPORT OF PENDING BILLS"
         return render(request, 'laws/bills_report.html',
-                      {'user': user, 'laws': laws, 'title': title})
+                      {'user': user, 'laws': laws, 'law_count': law_count, 'assented_laws_count': assented_laws_count,
+                       'pending_laws_count': pending_laws_count, 'title': title})
     else:
         return redirect('login')
 
@@ -111,8 +178,65 @@ def print_assented_laws(request):
         user = request.user
         laws = BillsAndLaws.objects.filter(stage="ASSENTED TO").order_by(F('assent_date').desc(nulls_last=True),
                                                                          F('stage_key').desc(nulls_last=True))
+        law_count = []
+        assented_laws_count = []
+        pending_laws_count = []
+        chart_label = []
+        base_year = 1995
+        assemblies = math.ceil((timezone.now().year - base_year) / 4)
+        print(assemblies)
+        for assembly in range(assemblies):
+            assembly_start = base_year + (4 * assembly)
+            assembly_end = assembly_start + 4
+            print(f'{assembly_start} to {assembly_end}')
+
+            law_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                         publication__year__lte=assembly_end).count())
+            pending_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                  publication__year__lte=assembly_end).exclude(
+                stage='ASSENTED TO').count())
+            assented_laws_count.append(BillsAndLaws.objects.filter(publication__year__gte=assembly_start,
+                                                                   publication__year__lte=assembly_end).filter(
+                stage='ASSENTED TO').count())
+            chart_label.append(assembly + 1)
+        print(chart_label)
         title = "DETAILED REPORT OF ASSENTED LAWS"
         return render(request, 'laws/bills_report.html',
-                      {'user': user, 'laws': laws, 'title': title})
+                      {'user': user, 'laws': laws, 'law_count': law_count, 'assented_laws_count': assented_laws_count,
+                       'pending_laws_count': pending_laws_count, 'chart_label': chart_label, 'title': title})
+    else:
+        return redirect('login')
+
+
+def get_law_by_assembly(request):
+    if request.user.is_authenticated:
+        user = request.user
+        laws = []
+        law_count = []
+        assented_laws_count = []
+        pending_laws_count = []
+        base_year = 1995
+        assemblies = math.ceil((timezone.now().year - base_year) / 4)
+        print(assemblies)
+        for assembly in range(assemblies):
+            assembly_start = base_year + (4 * assembly)
+            assembly_end = assembly_start + 4
+            print(f'{assembly_start} to {assembly_end}')
+            laws = BillsAndLaws.objects.filter(assent_date__year__gte=assembly_start,
+                                               assent_date__year__lte=assembly_end).order_by(
+                                                F('assent_date').desc(nulls_last=True),
+                                                F('stage_key').desc(nulls_last=True))
+
+            law_count.append(BillsAndLaws.objects.filter(assent_date__year__gte=assembly_start,
+                                                         assent_date__year__lte=assembly_end).count())
+            pending_laws_count.append(BillsAndLaws.objects.filter(assent_date__year__gte=assembly_start,
+                                                                  assent_date__year__lte=assembly_end).exclude(
+                                                                    stage='ASSENTED TO').count())
+            assented_laws_count.append(BillsAndLaws.objects.filter(assent_date__year__gte=assembly_start,
+                                                                   assent_date__year__lte=assembly_end).filter(
+                                                                    stage='ASSENTED TO').count())
+        return render(request, 'laws/list.html',
+                      {'user': user, 'assented_laws': laws, 'law_count': law_count,
+                       'pending_laws_count': pending_laws_count, 'assented_laws_count': assented_laws_count})
     else:
         return redirect('login')
